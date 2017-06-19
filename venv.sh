a632e819f294191f0f25c28c8bfaa58e300d3558
Bruce Eckel <mindviewinc@gmail.com>
2017-06-19 09:46:55 -0600

    Some fixes, still one issue

--- venv.sh
+++ venv.sh
@@ -6,19 +6,19 @@
 set -e pipefail
 
 # Works even if you're outside the starting directory:
-if [[ -x "${VIRTUAL_ENV}" ]]; then
+if [[ -v "${VIRTUAL_ENV}" ]]; then
     echo "Deactivating"
     deactivate
 else
     # Only works inside starting directory, otherwise
     # creates a new virtual environment:
-    if [[ -n ./virtualenv/bin/activate ]]; then
+    if [[ -e ./virtualenv/bin/activate ]]; then
         echo "Activating"
-        source ./virtualenv/bin/activate
+        source ./virtualenv/bin/activate # How to make this affect the calling shell?
     else
         echo "Initializing new virtualenv"
         python3 -m venv virtualenv
-        source ./virtualenv/bin/activate
+        source ./virtualenv/bin/activate # How to make this affect the calling shell?
         pip install --editable .
     fi
 fi
