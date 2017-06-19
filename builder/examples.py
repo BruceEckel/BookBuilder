9f7b81b636fc2d9dee197433abdaf564b6abc0c1
Bruce Eckel <mindviewinc@gmail.com>
2017-06-19 10:24:59 -0600

    First cut of code extraction working

--- builder/examples.py
+++ builder/examples.py
@@ -10,13 +10,96 @@ import sys
 from logging import debug
 from pathlib import Path
 
-import builder.config
+import builder.config as config
 
-def test():
-    print("AtomicKotlinBuilder examples.py")
 
-logging.basicConfig(filename=__file__.split(
-    '.')[0] + ".log", filemode='w', level=logging.DEBUG)
+logging.basicConfig(filename=__file__.split('.')[0] + ".log", filemode='w', level=logging.DEBUG)
+
+
+def clean():
+    "Remove directory containing extracted example code"
+    try:
+        if config.example_dir.exists():
+            shutil.rmtree(str(config.example_dir))
+            return "Removed: {}".format(config.example_dir)
+        else:
+            return "Doesn't exist: {}".format(config.example_dir)
+    except:
+        print("Old path removal failed")
+        raise RuntimeError()
+
+
+def extractExamples():
+    print("Extracting examples ...")
+    if not config.example_dir.exists():
+        debug("creating {}".format(config.example_dir))
+        config.example_dir.mkdir()
+    # copyTestFiles()
+
+    # for f in tools_to_copy:
+    #     shutil.copy(str(f), str(config.example_dir))
+
+    if not config.markdown_dir.exists():
+        return "Cannot find {}".format(config.markdown_dir)
+
+    slugline = re.compile("^(//|#) .+?\.[a-z]+$", re.MULTILINE)
+    xmlslug = re.compile("^<!-- .+?\.[a-z]+ +-->$", re.MULTILINE)
+
+    for sourceText in config.markdown_dir.glob("*.md"):
+        debug("--- {} ---".format(sourceText.name))
+        with sourceText.open("rb") as chapter:
+            text = chapter.read().decode("utf-8", "ignore")
+            for group in re.findall("```(.*?)\n(.*?)\n```", text, re.DOTALL):
+                listing = group[1].splitlines()
+                title = listing[0]
+                if slugline.match(title) or xmlslug.match(title):
+                    debug(title)
+                    fpath = title.split()[1].strip()
+                    target = config.example_dir / fpath
+                    debug("writing {}".format(target))
+                    if not target.parent.exists():
+                        target.parent.mkdir(parents=True)
+                    with target.open("w", newline='') as codeListing:
+                        debug(group[1])
+                        if slugline.match(title):
+                            codeListing.write(group[1].strip() + "\n")
+                        elif xmlslug.match(title):  # Drop the first line
+                            codeListing.write("\n".join(listing[1:]))
+
+    return "Code extracted into {}".format(config.example_dir)
+
+
+def copyGradleFiles():
+    print("Copying Gradle Files ...")
+    if not config.github_code_dir.exists():
+        print("Doesn't exist: %s" % config.github_code_dir)
+        sys.exit(1)
+    for gradle_path in list(config.github_code_dir.rglob("*gradle*")) + \
+            list(config.github_code_dir.rglob("*.xml")) + \
+            list(config.github_code_dir.rglob("*.yml")) + \
+            list(config.github_code_dir.rglob("*.md")) + \
+            list((config.github_code_dir / "buildSrc").rglob("*")):
+        dest = config.example_dir / \
+            gradle_path.relative_to(config.github_code_dir)
+        if gradle_path.is_file():
+            if(not dest.parent.exists()):
+                debug("creating " + str(dest.parent))
+                os.makedirs(str(dest.parent))
+            debug("copy " + str(gradle_path.relative_to(config.github_code_dir.parent)
+                                ) + " " + str(dest.relative_to(config.example_dir)))
+            shutil.copy(str(gradle_path), str(dest))
+
+
+def extractAndCopyBuildFiles():
+    "Clean, then extract examples from Markdown, copy gradle files from AtomicKotlin-Examples"
+    clean()
+    extractExamples()
+    copyGradleFiles()
+
+
+if __name__ == '__main__':
+    CmdLine.run()
+
 
 # For Development:
 tools_to_copy = [Path(sys.path[0]) / f for f in [
@@ -45,86 +128,3 @@ def copyTestFiles():
             shutil.copy(str(test_path), str(dest))
 
 
-maindef = re.compile("public\s+static\s+void\s+main")
-
-
-def extractExamples():
-    print("Extracting examples ...")
-    if not config.example_dir.exists():
-        debug("creating {}".format(config.example_dir))
-        config.example_dir.mkdir()
-    # copyTestFiles()
-
-    for f in tools_to_copy:
-        shutil.copy(str(f), str(config.example_dir))
-
-    if not config.markdown_dir.exists():
-        print("Cannot find", config.markdown_dir)
-        sys.exit()
-
-    slugline = re.compile("^(//|#) .+?\.[a-z]+$", re.MULTILINE)
-    xmlslug = re.compile("^<!-- .+?\.[a-z]+ +-->$", re.MULTILINE)
-
-    for sourceText in config.markdown_dir.glob("*.md"):
-        debug("--- {} ---".format(sourceText.name))
-        with sourceText.open("rb") as chapter:
-            text = chapter.read().decode("utf-8", "ignore")
-            for group in re.findall("```(.*?)\n(.*?)\n```", text, re.DOTALL):
-                listing = group[1].splitlines()
-                title = listing[0]
-                if slugline.match(title) or xmlslug.match(title):
-                    debug(title)
-                    fpath = title.split()[1].strip()
-                    target = config.example_dir / fpath
-                    debug("writing {}".format(target))
-                    if not target.parent.exists():
-                        target.parent.mkdir(parents=True)
-                    with target.open("w", newline='') as codeListing:
-                        debug(group[1])
-                        if slugline.match(title):
-                            codeListing.write(group[1].strip() + "\n")
-                        elif xmlslug.match(title):  # Drop the first line
-                            codeListing.write("\n".join(listing[1:]))
-
-
-def copyGradleFiles():
-    print("Copying Gradle Files ...")
-    if not config.github_code_dir.exists():
-        print("Doesn't exist: %s" % config.github_code_dir)
-        sys.exit(1)
-    for gradle_path in list(config.github_code_dir.rglob("*gradle*")) + \
-            list(config.github_code_dir.rglob("*.xml")) + \
-            list(config.github_code_dir.rglob("*.yml")) + \
-            list(config.github_code_dir.rglob("*.md")) + \
-            list((config.github_code_dir / "buildSrc").rglob("*")):
-        dest = config.example_dir / \
-            gradle_path.relative_to(config.github_code_dir)
-        if gradle_path.is_file():
-            if(not dest.parent.exists()):
-                debug("creating " + str(dest.parent))
-                os.makedirs(str(dest.parent))
-            debug("copy " + str(gradle_path.relative_to(config.github_code_dir.parent)
-                                ) + " " + str(dest.relative_to(config.example_dir)))
-            shutil.copy(str(gradle_path), str(dest))
-
-
-def clean():
-    "Remove ExtractedExamples directory"
-    print("Cleaning ...")
-    try:
-        if config.example_dir.exists():
-            shutil.rmtree(str(config.example_dir))
-    except:
-        print("Old path removal failed")
-        raise RuntimeError()
-
-
-def extractAndCopyBuildFiles():
-    "Clean, then extract examples from Markdown, copy gradle files from AtomicKotlin-Examples"
-    clean()
-    extractExamples()
-    copyGradleFiles()
-
-
-if __name__ == '__main__':
-    CmdLine.run()
--- builder/scripts/atomic_kotlin_builder.py
+++ builder/scripts/atomic_kotlin_builder.py
@@ -21,60 +21,66 @@ def code():
     """Code extraction and testing."""
 
 
-@code.command()
-def test():
-    """Testing use of another package in the script"""
-    builder.examples.test()
+@code.command('clean')
+def code_clean():
+    "Remove directory containing extracted example code"
+    click.echo(builder.examples.clean())
 
 
-@code.command('delete')
-def code_delete():
-    """Removes a code at a specific coordinate."""
-    click.echo("code directory removed")
+@code.command('extract')
+def code_extract():
+    "Extract examples from book's Markdown files"
+    click.echo(builder.examples.extractExamples())
 
 
-@code.command('set')
-@click.argument('x', type=float)
-@click.argument('y', type=float)
-@click.option('ty', '--moored', flag_value='moored',
-              default=True,
-              help='Moored (anchored) code. Default.')
-@click.option('ty', '--drifting', flag_value='drifting',
-              help='Drifting code.')
-def code_set(x, y, ty):
-    """Sets a code at a specific coordinate."""
-    click.echo('Set %s code at %s,%s' % (ty, x, y))
+# @code.command('set')
+# @click.argument('x', type=float)
+# @click.argument('y', type=float)
+# @click.option('ty', '--moored', flag_value='moored',
+#               default=True,
+#               help='Moored (anchored) code. Default.')
+# @click.option('ty', '--drifting', flag_value='drifting',
+#               help='Drifting code.')
+# def code_set(x, y, ty):
+#     """Sets a code at a specific coordinate."""
+#     click.echo('Set %s code at %s,%s' % (ty, x, y))
 
 
 ##########################################################
 
 @cli.group()
 def epub():
-    """Manages epubs."""
+    "Creates Atomic Kotlin epub"
 
 
-@epub.command('new')
-@click.argument('name')
-def epub_new(name):
-    """Creates a new epub."""
-    click.echo('Created epub %s' % name)
+@epub.command('clean')
+def epub_clean():
+    "Remove directory containing epub"
+    click.echo("Not implemented yet")
 
 
-@epub.command('move')
-@click.argument('epub')
-@click.argument('x', type=float)
-@click.argument('y', type=float)
-@click.option('--speed', metavar='KN', default=10,
-              help='Speed in knots.')
-def epub_move(epub, x, y, speed):
-    """Moves epub to the new location X,Y."""
-    click.echo('Moving epub %s to %s,%s with speed %s' % (epub, x, y, speed))
+# @epub.command('new')
+# @click.argument('name')
+# def epub_new(name):
+#     """Creates a new epub."""
+#     click.echo('Created epub %s' % name)
 
 
-@epub.command('shoot')
-@click.argument('epub')
-@click.argument('x', type=float)
-@click.argument('y', type=float)
-def epub_shoot(epub, x, y):
-    """Makes epub fire to X,Y."""
-    click.echo('epub %s fires to %s,%s' % (epub, x, y))
+# @epub.command('move')
+# @click.argument('epub')
+# @click.argument('x', type=float)
+# @click.argument('y', type=float)
+# @click.option('--speed', metavar='KN', default=10,
+#               help='Speed in knots.')
+# def epub_move(epub, x, y, speed):
+#     """Moves epub to the new location X,Y."""
+#     click.echo('Moving epub %s to %s,%s with speed %s' % (epub, x, y, speed))
+
+
+# @epub.command('shoot')
+# @click.argument('epub')
+# @click.argument('x', type=float)
+# @click.argument('y', type=float)
+# def epub_shoot(epub, x, y):
+#     """Makes epub fire to X,Y."""
+#     click.echo('epub %s fires to %s,%s' % (epub, x, y))
