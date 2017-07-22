9f5961c510d20a90d38d94fb96c9491e779ea435
Bruce Eckel <mindviewinc@gmail.com>
2017-07-22 11:52:07 -0600

    Generifying config.py

--- book_builder/config.py
+++ book_builder/config.py
@@ -3,27 +3,23 @@ Common program configuration variables for Book Builder,
 to target the Atomic Kotlin book. Each different book
 will only differ by this configuration file, so you create
 a different one for each book.
-
-This file lives in the home directory of the book project.
 """
 import os
 import sys
 from pathlib import Path
 
 base_name = "AtomicKotlin"
+root_name = "atomickotlin"
+language_name = "kotlin"
+
 epub_file_name = base_name + ".epub"
 epub_sample_file_name = base_name + "Sample.epub"
 
 code_width = 56
 
-try:
-    tools_dir = Path(os.environ['ATOMIC_KOTLIN_BUILDER'])
-except:
-    print("Error: need to set ATOMIC_KOTLIN_BUILDER")
-    sys.exit(1)
-
-root_path = tools_dir.parent / "AtomicKotlin"
-bb_code_dir = tools_dir / "atomic_kotlin_builder"
+tools_dir = Path(__file__).parent.parent.resolve()
+root_path = tools_dir.parent / base_name
+bb_code_dir = tools_dir / "book_builder"
 markdown_dir = root_path / "Markdown"
 example_dir = root_path / "ExtractedExamples"
 
@@ -32,14 +28,14 @@ html_dir = ebook_build_dir / "html"
 images_dir = ebook_build_dir / "images"
 test_dir = root_path / "test"
 
-combined_markdown = ebook_build_dir / "atomickotlin-assembled.md"
-combined_markdown_html = ebook_build_dir / "atomickotlin-assembled-html.md"
-combined_markdown_pdf = ebook_build_dir / "atomickotlin-assembled-pdf.md"
-stripped_for_style = ebook_build_dir / "atomickotlin-stripped-for-style.md"
+combined_markdown = ebook_build_dir / (root_name +"-assembled.md")
+combined_markdown_html = ebook_build_dir / (root_name +"-assembled-html.md")
+combined_markdown_pdf = ebook_build_dir / (root_name +"-assembled-pdf.md")
+stripped_for_style = ebook_build_dir / (root_name +"-stripped-for-style.md")
 stripped_for_spelling = ebook_build_dir / \
-    "atomickotlin-stripped-for-spelling.md"
-kotlin_code_only = ebook_build_dir / "atomickotlin-kotlin-code-only.md"
-kotlin_comments_only = ebook_build_dir / "atomickotlin-kotlin-comments-only.md"
+    (root_name +"-stripped-for-spelling.md")
+kotlin_code_only = ebook_build_dir / (root_name +"-" + language_name + "-code-only.md")
+kotlin_comments_only = ebook_build_dir / (root_name +"-" + language_name + "-comments-only.md")
 
 recent_atom_names = bb_code_dir / "recent_atom_names.py"
 
@@ -47,11 +43,11 @@ ebookResources = root_path / "resources"
 images = ebookResources / "images"
 fonts = ebookResources / "fonts"
 cover = ebookResources / "cover.jpg"
-css = ebookResources / "atomickotlin.css"
+css = ebookResources / (root_name +".css")
 metadata = ebookResources / "metadata.yaml"
 
 reformat_dir = root_path / "Reformatted"
 
 sample_book_dir = root_path / "SampleBook"
 sample_book_original_dir = root_path / "SampleBook" / "Original"
-combined_markdown_sample = sample_book_dir / "atomickotlin-assembled.md"
+combined_markdown_sample = sample_book_dir / (root_name +"-assembled.md")
