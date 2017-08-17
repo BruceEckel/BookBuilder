756661c18702377362719e8240b12844f704d666
Bruce Eckel <mindviewinc@gmail.com>
2017-08-17 16:31:41 -0600

    remove test print

--- book_builder/epub.py
+++ book_builder/epub.py
@@ -78,7 +78,6 @@ def disassemble_combined_markdown_file(target_dir=config.markdown_dir):
     if not target_dir.exists():
         target_dir.mkdir()
     for i, p in enumerate(chaps):
-        print(p)
         disassembled_file_name = create_numbered_markdown_filename(p, i)
         print(disassembled_file_name)
         dest = target_dir / disassembled_file_name
