86e4158a7c87e20b2f62ade2597d214b5f1ebef2
Bruce Eckel <mindviewinc@gmail.com>
2019-11-20 23:48:46 -0700

    Update validator_generator.py

--- book_builder/validator_generator.py
+++ book_builder/validator_generator.py
@@ -6,12 +6,12 @@ def create_validation_command(validator):
 @click.option('--trace', default="")
 def {validator.command_name}(trace):
     "{validator.__doc__.strip()}"
-    click.echo(_validate.Validator.one_check({validator.name()}, trace))
+    click.echo(_validate.Validator.one_check(_validate.{validator.name()}, trace))
 
 """
     return function_string
 
 
 if __name__ == '__main__':
-    for v in [v("") for v in Validator.__subclasses__()]:
-        print(create_validation_command(v))
+    cmds = "".join([create_validation_command(v("")) for v in Validator.__subclasses__()])
+    print(cmds)
