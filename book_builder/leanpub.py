6aaeed0983c49665d9cbb91d92288d32dfec5cca
Bruce Eckel <mindviewinc@gmail.com>
2020-04-22 16:36:22 -0600

    Update leanpub.py
    shutil.rmtree(manuscript_dir, ignore_errors=True)

--- book_builder/leanpub.py
+++ book_builder/leanpub.py
@@ -14,7 +14,7 @@ def update_leanpub_repo():
     if not leanpub_repo.exists():
         return f"Cannot find {leanpub_repo}"
     if manuscript_dir.exists():
-        shutil.rmtree(manuscript_dir)
+        shutil.rmtree(manuscript_dir, ignore_errors=True)
     shutil.copytree(config.markdown_dir, manuscript_dir)
     (manuscript_dir / "Book.txt").write_text(
         "\n".join([md.name for md in config.markdown_dir.glob("*.md")]).strip())
